5e6a67bb727c2c9b3acb1be2fb4367ed7a616d9c
ClaudiaVanessa969 <claudia-vanessa-1@hotmail.com>
2019-09-04 02:04:12 -0500

    Vectores

---
+++ Taller1/Punto2.py
@@ -0,0 +1,5 @@
+#Development: Luisa Bolaños - Claudia Urrea
+#Punto 2: Cree un algoritmo que imprima por pantalla los 20 primero términos de la sucesión Fibonacci.
+
+import os
+
--- vector2.py
+++ vector2.py
@@ -1,12 +1,33 @@
+''' Generar un vector de números alearotios de n longitud '''
 import os
 from random import randint, uniform, random
+ 
+def menu() :
+    os.system("cls")
+    print("[1] Generar vector de números aleatorios")
+    print("[2] Mostrar valores del vector")
+    print("[3] Primer valor del vector")
+    print("[4] Último valor del vector")
+    print("[5] Promedio de los números")
+    print("[6] Salir")
+    print("")
+    print("Escoja una opción: ")
+    opcion = int(input())
 
-N = []
+V = []
+i = 0
+suma = 0
+print("¿Cuántos números aleatorios desea generar?")
+n = int(input())
 
-print("Ingrese la cantidad de números: ")
-x = int(input())
+while i < n:
+    a = randint(0,100)
+    V.append(a)
+    suma = suma + a
+    i = i + 1
 
-def Z():
-    a = randint([0,100])
-    return a
-
+print("*****RESULTADO*****")
+print("1) Vector generado: ", V)
+print("2) Primer valor del vector: ", V[0])
+print("3) Último valor del vector: ", V[n-1])
+print("4) Promedio de los números: ", (suma/n))
