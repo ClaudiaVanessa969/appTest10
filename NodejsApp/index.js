4c4ae4f31a58f2c8bc4cf91841ac1c27ddf01054
ClaudiaVanessa969 <claudia-vanessa-1@hotmail.com>
2019-11-13 10:19:23 -0500

    NodejsApp

--- NodejsApp/index.js
+++ NodejsApp/index.js
@@ -30,11 +30,47 @@ mysqlConnection.connect((err) => {
 app.listen(3000, ()=>console.log(':::Express server is running at port: 3000:::'));
 
 //9. Read all users in data base
-app.get('/list_users',(req,res) => {
+app.get('/users',(req,res) => {
     mysqlConnection.query('SELECT * FROM users',(err, rows, fields)=>{
         if(!err){
             console.log(rows);
             res.send(rows);
+        }else{
+            console.log(err);
+        }
+    })
+});
+
+//10. Read an user in data base
+app.get('/users/:id',(req,res) => {
+    mysqlConnection.query('SELECT * FROM users WHERE id = ?',[req.params.id],(err, rows, fields)=>{
+        if(!err){
+            console.log(rows);
+            res.send(rows);
+        }else{
+            console.log(err);
+        }
+    })
+});
+
+//11. DELETE an user in data base
+app.delete('/users/:id',(req,res) => {
+    mysqlConnection.query('DELETE FROM users WHERE id = ?',[req.params.id],(err, rows, fields)=>{
+        if(!err){
+            console.log("User has been deleted");
+            res.send("User has been deleted");
+        }else{
+            console.log(err);
+        }
+    })
+});
+
+//11. Register a new user in data base
+app.post('/users',(req,res) => {
+    mysqlConnection.query('INSERT INTO users (email) VALUES (?)',[req.params.id],(err, rows, fields)=>{
+        if(!err){
+            console.log("A new user has been created");
+            res.send("A new user has been created");
         }else{
             console.log(err);
         }
